ec475ab9779c92f680bb7792df2ed12b2c03bddf
Ashish Tyagi <ashishtyagi10@gmail.com>
2020-12-27 10:24:07 -0500

    added some more styling

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CateogyMealScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+        title: Text('Meal Screen'),
+      ),
+      body: Container(
+        child: Text('Category Meal Screen'),
+      ),
+    );
+  }
+}
